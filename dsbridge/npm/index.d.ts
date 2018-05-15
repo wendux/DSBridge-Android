5d8d8adfdeb5f4f64cff7f085cc9d5554f7c3cab
imconfig <biluochun@users.noreply.github.com>
2018-05-15 11:46:10 +0800

    Update index.d.ts

--- dsbridge/npm/index.d.ts
+++ dsbridge/npm/index.d.ts
@@ -1,17 +1,15 @@
 interface DSBridge {
-    call(handlerName: string, args?: any, responseCallback?: (retValue: any) => void): any;
-    call<T, R>(handlerName: string, args?: T, responseCallback?: (retValue: R) => void): R;
+  call (handlerName: string, args?: any, responseCallback?: (retValue: any) => void): any;
+  call<T, R> (handlerName: string, args?: T, responseCallback?: (retValue: R) => void): R;
 
-    register(handlerName: string, handler: () => any, async?: boolean): void;
-    register<F>(handlerName: string, handler: F, async?: boolean): void;
-    register(namespace: string, synApiObject: object, async?: boolean): void;
+  register (handlerName: string, handler: object | (() => any), async?: boolean): void;
+  register<F> (handlerName: string, handler: F, async?: boolean): void;
 
-    registerAsyn(handlerName: string, handler: () => void): void;
-    registerAsyn<F>(handlerName: string, handler: F): void;
-    registerAsyn(namespace: string, asyApiObject: object): void;
+  registerAsyn (handlerName: string, handler: object | (() => void)): void;
+  registerAsyn<F> (handlerName: string, handler: F): void;
 
-    hasNativeMethod(handlerName: string, type?: ("all"|"asyn"|"syn")): boolean;
-    disableJavascriptDialogBlock(disable?: boolean): void;
+  hasNativeMethod (handlerName: string, type?: ('all' | 'asyn' | 'syn')): boolean;
+  disableJavascriptDialogBlock (disable?: boolean): void;
 }
 
 declare const bridge: DSBridge;
