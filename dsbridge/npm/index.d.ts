8c1680eb64b5b0fe94b1b520c84ddfb7afd79fa1
imconfig <biluochun@users.noreply.github.com>
2018-05-15 11:41:25 +0800

    Update index.d.ts

--- dsbridge/npm/index.d.ts
+++ dsbridge/npm/index.d.ts
@@ -1,11 +1,19 @@
-interface DSBridge{
-    call(handlerName:string,args?:any,responseCallback?:(retValue:any)=>void);
-    register(handlerName:string,handler:()=>any);
-    register(namespace:string,synApiObject:object);
-    registerAsyn(handlerName:string,handler:()=>void);
-    registerAsyn(namespace:string,asyApiObject:object);
-    hasNativeMethod(handlerName:string,type?:("all"|"asyn"|"syn"));
-    disableJavascriptDialogBlock(disable?:boolean);
+interface DSBridge {
+    call(handlerName: string, args?: any, responseCallback?: (retValue: any) => void): any;
+    call<T, R>(handlerName: string, args?: T, responseCallback?: (retValue: R) => void): R;
+
+    register(handlerName: string, handler: () => any, async?: boolean): void;
+    register<F>(handlerName: string, handler: F, async?: boolean): void;
+    register(namespace: string, synApiObject: object, async?: boolean): void;
+
+    registerAsyn(handlerName: string, handler: () => void): void;
+    registerAsyn<F>(handlerName: string, handler: F): void;
+    registerAsyn(namespace: string, asyApiObject: object): void;
+
+    hasNativeMethod(handlerName: string, type?: ("all"|"asyn"|"syn")): boolean;
+    disableJavascriptDialogBlock(disable?: boolean): void;
 }
-declare const bridge:DSBridge;
+
+declare const bridge: DSBridge;
+
 export default bridge;
